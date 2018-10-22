f476fec5f7677ba533f7b2fecc61931b2b0d6703
Axel <skullcandyapple@gmail.com>
2018-10-22 13:07:04 +0200

    Updated track.js to java-like class

--- track.js
+++ track.js
@@ -1,55 +1,57 @@
-function Track (w, h, map) {
-  this.width = w;
-  this.height = h;
-
-  for (let i = 0; i < map.length; i++) {
-    if (map[i].length <= 1) {
-      map.pop(i);
-      continue;
-    }
-    map[i] = map[i].split(" ");
-    if (i > 0) {
-      if (map[i].length > map[0].length) {
-        for (let q = 0; q < map[i].length - map[0].length + 2; q++) {
-          map[i].pop();
-        }
-      }else if (map[i].length < map[0].length) {
-        for (let q = 0; q < map[0].length - map[i].length + 1; q++) {
-          map[i].push("X");
-        }
-      }
-    }
+class Track {
+  constructor(w, h, map) {
+    this.width = w;
+    this.height = h;
+    this.grid = Track.parseMap(map, w, h);
+    this.x = this.grid.length;
+    this.y = this.grid[0].length;
+    this.blockWidth = w / this.x;
+    this.blockHeight = h / this.y;
   }
 
-  this.grid = transpose(map);
-  this.x = map.length;
-  this.y = map[0].length;
-  this.blockWidth = w / this.x;
-  this.blockHeight = h / this.y;
-
-  for (let x = 0; x < this.grid.length; x++) {
-    for (let y = 0; y < this.grid[0].length; y++) {
-      if (this.grid[x][y] === "X"){
-        this.grid[x][y] = new Entity(createVector(this.blockWidth*x,this.blockHeight*y), "space");
-      }
-      if (this.grid[x][y] === "S" || this.grid[x][y] === "<" || this.grid[x][y] === ">" || this.grid[x][y] === "V" || this.grid[x][y] === "^"){
-        this.grid[x][y] = new Entity(createVector(this.blockWidth*x,this.blockHeight*y), "road");
-      }
-    }
-  }
-
-  this.draw = function() {
-    for (let x = 0; x < this.grid.length; x++) {
-      for (let y = 0; y < this.grid[0].length; y++) {
+  draw() {
+    for (let x = 0; x < this.x; x++) {
+      for (let y = 0; y < this.y; y++) {
         this.grid[x][y].draw(this.blockWidth, this.blockHeight);
       }
     }
   }
-}
 
-function transpose(a) {
-  // https://stackoverflow.com/a/13241545
-    return Object.keys(a[0]).map(function(c) {
-        return a.map(function(r) { return r[c]; });
-    });
+  static parseMap(map, w, h) {
+    for (let i = 0; i < map.length; i++) {
+      if (map[i].length <= 1) {
+        map.pop(i);
+        continue;
+      }
+      map[i] = map[i].split(" ");
+      if (i > 0) {
+        if (map[i].length > map[0].length) {
+          for (let q = 0; q < map[i].length - map[0].length + 2; q++) {
+            map[i].pop();
+          }
+        }else if (map[i].length < map[0].length) {
+          for (let q = 0; q < map[0].length - map[i].length + 1; q++) {
+            map[i].push("X");
+          }
+        }
+      }
+    }
+    map = transpose(map)
+
+    let blockWidth = w / map.length;
+    let blockHeight = h / map[0].length;
+
+    for (let x = 0; x < map.length; x++) {
+      for (let y = 0; y < map[0].length; y++) {
+        if (map[x][y] === "X"){
+          map[x][y] = new Entity(createVector(blockWidth*x,blockHeight*y), "space");
+        }
+        if (map[x][y] === "S" || map[x][y] === "<" || map[x][y] === ">" || map[x][y] === "V" || map[x][y] === "^"){
+          map[x][y] = new Entity(createVector(blockWidth*x,blockHeight*y), "road");
+        }
+      }
+    }
+
+    return map;
+  }
 }
