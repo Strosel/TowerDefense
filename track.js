0f942015a891475d4024d67e41381de82bb445fc
Axel <skullcandyapple@gmail.com>
2018-10-22 13:18:41 +0200

    track.js java-like class

--- track.js
+++ track.js
@@ -1,20 +1,22 @@
-function Track (w, h, x, y) {
-  this.width = w;
-  this.height = h;
-  this.x = x;
-  this.y = y
-  this.blockWidth = w / x;
-  this.blockHeight = h / y;
-  this.grid = [];
+class Track {
+  constructor(w, h, x, y) {
+    this.width = w;
+    this.height = h;
+    this.x = x;
+    this.y = y
+    this.blockWidth = w / x;
+    this.blockHeight = h / y;
+    this.grid = [];
 
-  for (let xx = 0; xx < x; xx++) {
-    this.grid.push([]);
-    for (let yy = 0; yy < y; yy++) {
-      this.grid[xx].push(new Entity(createVector(this.blockWidth*xx,this.blockHeight*yy), 0));
+    for (let xx = 0; xx < x; xx++) {
+      this.grid.push([]);
+      for (let yy = 0; yy < y; yy++) {
+        this.grid[xx].push(new Entity(createVector(this.blockWidth*xx,this.blockHeight*yy), 0));
+      }
     }
   }
 
-  this.Draw = function() {
+  draw() {
     for (let x = 0; x < this.x; x++) {
       for (let y = 0; y < this.y; y++) {
         currEntity = this.grid[x][y];
