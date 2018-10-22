0d14665aaca38c14a2b5b64702f8a20681449b81
Axel <skullcandyapple@gmail.com>
2018-10-22 13:53:43 +0200

    java-like classes and startpoint class

--- road.js
+++ road.js
@@ -1,5 +1,46 @@
-function Road(pos, tex, dir) {
-  Entity.call(this, pos, tex);
+class Road extends Entity {
+  constructor(pos, tex, dir) {
+    super(pos, tex);
 
-  this.direction = dir;
+    this.direction = dir;
+  }
+
+  draw(bw, bh) {
+    try {
+      if (this.direction.x === 1 && this.direction.y === 0) {
+        fill('green');
+        rect(this.position.x, this.position.y, bw, bh);
+      }else if (this.direction.x === -1 && this.direction.y === 0) {
+        fill('yellow');
+        rect(this.position.x, this.position.y, bw, bh);
+      }else if (this.direction.x === 0 && this.direction.y === 1) {
+        fill('orange');
+        rect(this.position.x, this.position.y, bw, bh);
+      }else if (this.direction.x === 0 && this.direction.y === -1) {
+        fill('pink');
+        rect(this.position.x, this.position.y, bw, bh);
+      }
+      // image(this.texture, this.position.x, this.position.y, bw, bh)
+    } catch (e) {
+      fill('red');
+      rect(this.position.x, this.position.y, bw, bh);
+    }
+  }
+}
+
+class StartPoint extends Road {
+  constructor(pos, tex, dir) {
+    super(pos, tex, dir);
+  }
+
+  draw(bw, bh) {
+    try {
+      fill('brown');
+      rect(this.position.x, this.position.y, bw, bh);
+      // image(this.texture, this.position.x, this.position.y, bw, bh)
+    } catch (e) {
+      fill('red');
+      rect(this.position.x, this.position.y, bw, bh);
+    }
+  }
 }
