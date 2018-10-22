25693e8c4e18a099b0a3d14e57041079c2a038de
Axel <skullcandyapple@gmail.com>
2018-10-22 12:55:07 +0200

    updated entity to java-like classes

--- entity.js
+++ entity.js
@@ -1,8 +1,10 @@
-function Entity(pos, tex) {
-  this.position = pos;
-  this.texture = tex;
+class Entity {
+  constructor(pos, tex) {
+    this.position = pos;
+    this.texture = tex;
+  }
 
-  this.draw = (bw, bh) => {
+  draw(bw, bh) {
     try {
       image(this.texture, this.position.x, this.position.y, bw, bh)
     } catch (e) {
@@ -12,8 +14,10 @@ function Entity(pos, tex) {
   }
 }
 
-function MovEntity(pos, tex, speed) {
-  Entity.call(this, pos, tex);
+class MovEntity extends Entity {
+  constructor(pos, tex, speed) {
+    super(pos, tex);
 
-  this.speed = speed;
+    this.speed = speed;
+  }
 }
