331ccc2230b7fddef5e5c17d4b3a290db5e08978
Axel <skullcandyapple@gmail.com>
2018-10-22 10:31:01 +0200

    added insividual draw methods and text to track parsing

--- entity.js
+++ entity.js
@@ -1,6 +1,15 @@
 function Entity(pos, tex) {
   this.position = pos;
   this.texture = tex;
+
+  this.draw = (bw, bh) => {
+    try {
+      image(this.texture, this.position.x, this.position.y, bw, bh)
+    } catch (e) {
+      fill('red');
+      rect(this.position.x, this.position.y, bw, bh);
+    }
+  }
 }
 
 function MovEntity(pos, tex, speed) {
--- track.js
+++ track.js
@@ -1,24 +1,44 @@
-function Track (w, h, x, y) {
+function Track (w, h, map) {
   this.width = w;
   this.height = h;
-  this.x = x;
-  this.y = y
-  this.blockWidth = w / x;
-  this.blockHeight = h / y;
-  this.grid = [];
 
-  for (let xx = 0; xx < x; xx++) {
-    this.grid.push([]);
-    for (let yy = 0; yy < y; yy++) {
-      this.grid[xx].push(new Entity(createVector(this.blockWidth*xx,this.blockHeight*yy), 0));
+  for (let i = 0; i < map.length; i++) {  // add parsing for lenght if map[0][x].lenght < map[0][0].lenght insert; map[0][x].lenght > map[0][0].lenght truncate
+    if (map[i].length <= 1) {
+      map.pop(i);
+      continue;
+    }
+    map[i] = map[i].split(" ");
+    if (i > 0) {
+      if (map[i].length > map[0].length) {
+        for (let q = 0; q < map[i].length - map[0].length + 2; q++) {
+          map[i].pop();
+        }
+      }else if (map[i].length < map[0].length) {
+        for (let q = 0; q < map[0].length - map[i].length + 1; q++) {
+          map[i].push("X");
+        }
+      }
     }
   }
 
-  this.Draw = function() {
-    for (let x = 0; x < this.x; x++) {
-      for (let y = 0; y < this.y; y++) {
-        currEntity = this.grid[x][y];
-        rect(currEntity.position.x, currEntity.position.y, this.blockWidth, this.blockHeight);
+  this.grid = map;
+  this.x = map.length;
+  this.y = map[0].length;
+  this.blockWidth = w / this.x;
+  this.blockHeight = h / this.y;
+
+  for (let x = 0; x < this.grid.length; x++) {
+    for (let y = 0; y < this.grid[0].length; y++) {
+      if (this.grid[x][y] = "X"){
+        this.grid[x][y] = new Entity(createVector(this.blockWidth*x,this.blockHeight*y), "space");
+      }
+    }
+  }
+
+  this.draw = function() {
+    for (let x = 0; x < this.grid.length; x++) {
+      for (let y = 0; y < this.grid[0].length; y++) {
+        this.grid[x][y].draw(this.blockWidth, this.blockHeight);
       }
     }
   }
