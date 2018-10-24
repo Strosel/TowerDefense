e01153b83b9063e68f5c92b53e3cf19d0b03893b
edonc <git@cakolli.se>
2018-10-24 11:52:02 +0200

    Added enemy posistions

--- enemy.js
+++ enemy.js
@@ -2,6 +2,12 @@ class Enemy extends MovEntity {
   constructor (pos, tex, siz, speed, health) {
     super(pos, tex, siz, speed);
     this.health = health;
+
+    var origin = createVector(pos.x + size.x / 2, pos.y + size.y / 2);
+    var top = createVector(pos.x + size.x / 2, pos.y);
+    var right = createVector(pos.x + size.x, pos.y + size.y / 2);
+    var bottom = createVector(pos.x + size.x / 2, pos.y + size.y);
+    var left = createVector(pos.x, pos.y + size.y / 2);
   }
 
   move() {
