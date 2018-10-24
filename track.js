cae0b731b2fdb6e26004201504034a9cc51bf778
Axel <skullcandyapple@gmail.com>
2018-10-24 12:22:44 +0200

    added size to colored roads

--- track.js
+++ track.js
@@ -61,15 +61,15 @@ class Track {
     for (let x = 0; x < map.length; x++) {
       for (let y = 0; y < map[0].length; y++) {
         if (map[x][y] === "X"){
-          map[x][y] = new Entity(createVector(blockWidth*x,blockHeight*y), "space");
+          map[x][y] = new Entity(createVector(blockWidth*x,blockHeight*y), "space", entitySize);
         }else if (map[x][y] === "<"){
-          map[x][y] = new Road(createVector(blockWidth*x,blockHeight*y), "road", createVector(-1, 0));
+          map[x][y] = new Road(createVector(blockWidth*x,blockHeight*y), "road", entitySize, createVector(-1, 0));
         }else if (map[x][y] === ">"){
-          map[x][y] = new Road(createVector(blockWidth*x,blockHeight*y), "road", createVector(1, 0));
+          map[x][y] = new Road(createVector(blockWidth*x,blockHeight*y), "road", entitySize, createVector(1, 0));
         }else if (map[x][y] === "V"){
-          map[x][y] = new Road(createVector(blockWidth*x,blockHeight*y), "road", createVector(0, 1));
+          map[x][y] = new Road(createVector(blockWidth*x,blockHeight*y), "road", entitySize, createVector(0, 1));
         }else if (map[x][y] === "^"){
-          map[x][y] = new Road(createVector(blockWidth*x,blockHeight*y), "road", createVector(0, -1));
+          map[x][y] = new Road(createVector(blockWidth*x,blockHeight*y), "road", entitySize, createVector(0, -1));
         }else if (typeof map[x][y] === 'string') {
           map[x][y] = new Entity(createVector(blockWidth*x,blockHeight*y), "undefined");
           console.warn(`undefined entity in map at (${x};${y})`);
