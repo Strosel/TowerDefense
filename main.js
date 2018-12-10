44b04657e1603656fdf6b72f01a3955d22857a3a
Axel <axel.paulander@gmail.com>
2018-12-10 11:52:51 +0100

    oops

--- main.js
+++ main.js
@@ -19,9 +19,9 @@ function preload() {
   textures.roads.horizontal = loadImage("./assets/horizontal.png");
   textures.roads.vertical = loadImage("./assets/vertical.png");
   textures.roads.lt = loadImage("./assets/lt.png");
-  textures.roads.lb = loadImage("./assets/lb.");
+  textures.roads.lb = loadImage("./assets/lb.png");
   textures.roads.rt = loadImage("./assets/rt.png");
-  textures.roads.rb = loadImage("./assets/rb.");
+  textures.roads.rb = loadImage("./assets/rb.png");
   textures.towers.spritesheet = loadImage('./assets/tower.png');
 }
 
